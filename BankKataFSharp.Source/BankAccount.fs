f1065f1ac132be46139c4927a4337cd1f318cf7a
Alessandro Di Gioia <Alessandro Di Gioia>
2015-11-26 16:28:21 +0000

    Implemented Acceptance test

---
+++ BankKataFSharp.Source/BankAccount.fs
@@ -0,0 +1,21 @@
+﻿namespace BankKataFSharp.Source
+
+    open System
+
+//
+//    type Amount = Amount of int
+//
+//    type Transaction = 
+//        | Credit of DateTime * Amount
+//        | Debit of DateTime * Amount
+//
+//    type Transactions = Transactions of Transaction list
+    
+
+    type BankAccount() =
+        member x.deposit (amount:int) = ()
+        member x.withdraw (amount:int) = ()
+        member x.printStatement = ()
+
+    type IDisplay =
+        abstract member Show: string -> Unit 
---
+++ BankKataFSharp.Tests/BankAccountAcceptance.fs
@@ -0,0 +1,27 @@
+﻿namespace BankKataFSharp.Tests
+
+module BankAccountAcceptance =
+    open NUnit.Framework
+    open BankKataFSharp.Source
+    open NSubstitute
+
+    let console = Substitute.For<IDisplay>()
+    let bankAccount = new BankAccount()
+
+
+    [<Ignore>][<Test>]
+    let ignoreMe() = ()
+
+
+    [<Test>]
+    let ``it outputs the bank statement of the operations``() =
+        bankAccount.deposit 1000
+        bankAccount.withdraw 100
+        bankAccount.deposit 500
+
+        bankAccount.printStatement
+
+        console.Received(1).Show("DATE | AMOUNT | BALANCE")
+        console.Received(1).Show("10/04/2014 | 500.00 | 1400.00")
+        console.Received(1).Show("02/04/2014 | -100.00 | 900.00")
+        console.Received(1).Show("01/04/2014 | 1000.00 | 1000.00")
