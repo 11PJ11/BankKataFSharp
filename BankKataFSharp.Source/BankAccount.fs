821bb6c1f7581b03ed33fd000167de3d0f883f28
Alessandro Di Gioia <Alessandro Di Gioia>
2015-11-30 17:09:51 +0000

    implemented print header

--- BankKataFSharp.Source/BankAccount.fs
+++ BankKataFSharp.Source/BankAccount.fs
@@ -9,11 +9,17 @@ module BankAccount =
     type IDisplay = 
         abstract member show: string -> Unit
     
+    type IStatementPrinter =
+        abstract member printHeader: Unit -> Unit 
+
     type StatementPrinter(display:IDisplay) =
+        
         let HEADER = "DATE | AMOUNT | BALANCE" 
         let _display = display
-        member x.PrintHeader =
-            _display.show HEADER
+
+        interface IStatementPrinter with
+            member x.printHeader() =
+                _display.show HEADER
     
     type BankAccount = 
         | BankAccount of IClock
@@ -22,5 +28,5 @@ module BankAccount =
 
     let withdraw (amount : int) (account : BankAccount) : BankAccount = account
 
-    let printStatement (printer:StatementPrinter) (account:BankAccount) = 
-        printer.PrintHeader
+    let printStatement (printer:IStatementPrinter) (account:BankAccount) = 
+        printer.printHeader()
---
+++ BankKataFSharp.Tests/BankAccountShould.fs
@@ -0,0 +1,21 @@
+﻿module BankAccountShould
+
+open NUnit.Framework
+open NSubstitute
+open Swensen.Unquote
+open BankKataFSharp.Source.BankAccount
+
+let clock = Substitute.For<IClock>()
+let printer = Substitute.For<IStatementPrinter>()
+
+[<Test>]
+let ``print only the statement header given an empty bank account`` () = 
+    let bankAccount = BankAccount clock
+
+    bankAccount 
+    |> printStatement printer 
+    |> ignore 
+
+    printer.Received().printHeader()
+
+
